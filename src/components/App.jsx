f692c0336d8b1390baba3a068d860a2ce996a991
Louise Eriksson <louiseriksson.92@gmail.com>
2024-03-02 12:48:44 +0100

    working om css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import '../style/App.css'
+import Namecomp from './Namecomp'
 
 function App() {
 
   return (
     <>
+    <div className="header">
+      <Namecomp />
+    </div>
     </>
   )
 }
---
+++ src/components/Namecomp.jsx
@@ -0,0 +1,13 @@
+const Namecomp = () => {
+    return ( 
+        <div className="nameComp">
+            <div id="Name">
+                <h1>LOUISE ERIKSSON</h1>
+                <h2>Frontend Developer</h2>
+            </div>
+        </div>
+
+     );
+}
+ 
+export default Namecomp;
