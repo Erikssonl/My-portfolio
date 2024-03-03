45dc0dd48cc69453c08c85de51175701c0a0bae8
Louise Eriksson <louiseriksson.92@gmail.com>
2024-03-03 12:48:20 +0100

    img uplode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import '../style/App.css'
+import '../style/index.css'
 import Namecomp from './Namecomp'
 
 function App() {
--- src/components/Namecomp.jsx
+++ src/components/Namecomp.jsx
@@ -1,9 +1,12 @@
+import '../style/App.css'
+
 const Namecomp = () => {
 
     const name = "LOUISE ERIKSSON"
     const title = "Frontend Developer"
     return ( 
         <div className="nameComp">
+            <img src="/img/Group\2.jpg" alt="" />
             <div id="Name">
                 <h1>{name}</h1>
                 <h2>{title}</h2>
