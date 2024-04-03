7813bd504bbe383c2e479f190b5039d26f86e7d3
Louise Eriksson <louiseriksson.92@gmail.com>
2024-04-03 14:33:37 +0200

    text animation done

--- src/components/Aboutcomp.jsx
+++ src/components/Aboutcomp.jsx
@@ -1,8 +1,37 @@
+import React, { useEffect, useRef } from 'react';
 import cat from '../img/cat.jpg'
 import circle2 from '../img/circle2.jpg'
 import styles from "../style/Aboutcomp-style.module.css";
 
 const Aboutcomp = () => {
+  const textRef = useRef(null);
+
+  useEffect (()=> {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(styles.slideIn);
+          }
+        });
+      }, 
+      {
+        threshold: 0.5,
+      }
+    );
+    
+    if (textRef.current) {
+      observer.observe(textRef.current)
+    }
+
+    return () => {
+      if (textRef.current) {
+        observer.unobserve(textRef.current);
+      }
+    };
+
+  }, []);
+
   return (
     <div className="aboutComp">
         <h2 className={styles.aboutMe}>About me</h2>
@@ -11,7 +40,7 @@ const Aboutcomp = () => {
             <img className={styles.img} src={circle2} alt="" /> 
             <div className={styles.aboutSection}>
                 <img className={styles.me} src={cat} alt="" />
-                <p className={styles.text}>Hello, I am an Frontend Developer student seeking new and developmental challenges. I am often described as cheerful, positive, and proactive. I embrace new challenges and  I enjoy being creative. I always approach tasks with a positive mindset, aiming to become the best Frontend Developer I can be.</p>
+                <p className={styles.text} ref={textRef}>Hello, I am an Frontend Developer student seeking new and developmental challenges. I am often described as cheerful, positive, and proactive. I embrace new challenges and  I enjoy being creative. I always approach tasks with a positive mindset, aiming to become the best Frontend Developer I can be.</p>
             </div>
         </div>
     </div>
