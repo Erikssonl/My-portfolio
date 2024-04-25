0a071e8638d061194baa8c7b0dc95e4ce1adb95e
Louise Eriksson <louiseriksson.92@gmail.com>
2024-04-25 15:28:33 +0200

    some styling on the projectcomp

--- src/components/Projectcomp.jsx
+++ src/components/Projectcomp.jsx
@@ -1,28 +1,70 @@
 import styles from "../style/Projectcomp-style.module.css";
 import epicEats from '../img/epicEats.png';
 import portfolio from '../img/portfolio.png';
-import ceresDam from '../img/ceresDam.png';
 import candyHub from '../img/candyHub.png'
+import { useRef } from "react";
 
 const Projectcomp = () => {
+    const epicEatsRef = useRef(null);
+    const portfolioRef = useRef(null);
+    const candyHubRef = useRef(null);
+
+    const handleMouseMove = (e, ref) => {
+        const img = ref.current;
+        const boundingRect = img.getBoundingClientRect();
+
+        const offsetX = e.clientX - boundingRect.left - boundingRect.width / 2;
+        const offsetY = e.clientY - boundingRect.top - boundingRect.height / 2;
+        const tiltX = offsetX / boundingRect.width * 20;
+        const tiltY = offsetY / boundingRect.height * 20;
+
+        img.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
+    };
+
+    const handleMouseLeave = (ref) => {
+        const img = ref.current;
+        img.style.transform = 'none';
+    }
+
   return (
     <>
         <h2 className={styles.projects}>Projects</h2>
         <div className={styles.projectWrap}>
-            {/* <div className="epicEats"> */}
-                <img className={styles.epicEatsImg} src={epicEats} alt="" />
+            {/* <div className={styles.epicEats}> */}
+                <img 
+                className={styles.epicEatsImg} 
+                src={epicEats} 
+                alt="" 
+                ref={epicEatsRef}
+                onMouseMove={(e) => handleMouseMove(e, epicEatsRef)}
+                onMouseLeave={() => handleMouseLeave(epicEatsRef)}
+                />
                 <div className={styles.epicEatsInfo}>
                     <p></p>
                 </div>
             {/* </div> */}
-            {/* <div className="portfolio"> */}
+            {/* <div className={styles.portfolio}> */}
                 <div className={styles.portfolioInfo}>
                     <p></p>
                 </div>
-                <img className={styles.portfolioImg} src={portfolio} alt="" />
+                <img 
+                className={styles.portfolioImg} 
+                src={portfolio} 
+                alt="" 
+                ref={portfolioRef}
+                onMouseMove={(e) => handleMouseMove(e, portfolioRef)}
+                onMouseLeave={() => handleMouseLeave(portfolioRef)}
+                />
             {/* </div> */}
-            {/* <div className="ceresDam"> */}
-                <img className={styles.candyHubImg} src={candyHub} alt="" />
+            {/* <div className={styles.candyHub}> */}
+                <img 
+                className={styles.candyHubImg} 
+                src={candyHub} 
+                alt="" 
+                ref={candyHubRef}
+                onMouseMove={(e) => handleMouseMove(e, candyHubRef)}
+                onMouseLeave={() => handleMouseLeave(candyHubRef)}
+                />
                 <div className={styles.candyHubInfo}>
                     <p></p>
                 </div>
