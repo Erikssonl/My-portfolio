5ec758fbe34db8e1a41abb1ee5f17deb60c2efad
Louise Eriksson <louiseriksson.92@gmail.com>
2024-05-27 11:06:49 +0200

    skill update done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import NavbarComp from './NavbarComp';
 
 function App() {
 
-  const skills = ["HTML", "CSS", "React", "JS", "Github", "Figma"];
+  const skills = ["HTML", "CSS", "React", "JS", "Git", "Github", "Node", "API", "Figma", "Testing", "Cypress"];
   const infoEe = "EpicEats is an individual school project where users can search for food dishes and access recipes with ingredient lists and instructions. The application is built with React.js and utilizes MealDB's open API. I have designed the application's interface myself by starting the designwork in figma and then using CSS style-sheets"
   const infoP = "My portfolio is a personal project where I've built this website using React.js, aiming to serve as a presentation and representation of myself as both a person and a frontend developer. In this project, I've experimented with various design features and animations using CSS. This website will likely remain under constant development as I enjoy exploring new styles and functionalities to enhance my coding skills."
   const infoCh = "This project is a work in progress and will go live shortly"
